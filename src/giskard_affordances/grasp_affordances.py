e58020435ef0fae854af221b6d656d21aa4b5ccb
Adrian Röfer <aroefer@uni-bremen.de>
2017-10-24 21:45:07 -0400

    :unicorn: Successful adaptation to symengine

--- src/giskard_affordances/grasp_affordances.py
+++ src/giskard_affordances/grasp_affordances.py
@@ -11,17 +11,17 @@ def ball_grasp_probabilistic(gripper, obj_input):
 def cylinder_grasp_affordance(gripper, obj_input):
     frame = obj_input.get_frame()
     shape = obj_input.get_dimensions()
-    cylinder_z = frame.col(2)
+    cylinder_z = frame[:4, 2:3]
     cylinder_pos = pos_of(frame)
 
-    gripper_x = gripper.frame.col(0)
-    gripper_z = gripper.frame.col(2)
+    gripper_x = gripper.frame[:4, 0:1]
+    gripper_z = gripper.frame[:4, 2:3]
     gripper_pos = pos_of(gripper.frame)
     c_to_g = gripper_pos - cylinder_pos
 
-    zz_align = sp.Abs(gripper_z.dot(cylinder_z))
-    xz_align = gripper_x.dot(cylinder_z)
-    dist_z = cylinder_z.dot(c_to_g)
+    zz_align = sp.Abs(dot(gripper_z, cylinder_z))
+    xz_align = dot(gripper_x, cylinder_z)
+    dist_z = dot(cylinder_z, c_to_g)
     border_z = (shape[2] - gripper.height) * 0.5
     cap_dist_normalized_signed = dist_z / border_z
     cap_dist_normalized = sp.Abs(cap_dist_normalized_signed)
@@ -30,7 +30,7 @@ def cylinder_grasp_affordance(gripper, obj_input):
     cap_bottom_grasp = 1 - sp.Min(xz_align * sp.Max(cap_dist_normalized_signed, -1), 0)
 
     dist_z_center_normalized = sp.Max(1 - cap_dist_normalized, 0)
-    dist_ax = sp.sqrt(frame.col(0).dot(c_to_g)**2 + frame.col(1).dot(c_to_g)**2)
+    dist_ax = sp.sqrt(dot(frame[:4, 0:1], c_to_g)**2 + dot(frame[:4, 1:2], c_to_g)**2)
 
     center_grasp = (1 - dist_z_center_normalized - dist_ax) * zz_align
 
