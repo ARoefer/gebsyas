7c740a27640eb3571923bbc41cd05742d13454a6
Adrian Röfer <aroefer@uni-bremen.de>
2018-02-03 19:11:58 -0500

    More typos

--- src/giskard_affordances/spatial_relations.py
+++ src/giskard_affordances/spatial_relations.py
@@ -1,74 +1,89 @@
-from giskardpy.symengine_wrappers import *
 import symengine as sp
+from giskardpy.symengine_wrappers import *
+from giskardpy.qp_problem_builder import SoftConstraint as SC
 from giskard_affordances.dl_reasoning import *
 from giskard_affordances.utils import *
 
-class SpacialRelations(object):
+# Only works for x > 0 so beware
+def crappyAtan2(x, y):
+	return sp.atan(y/x)
+
+
+class SpatialRelations(object):
 
 	@classmethod
-	def at(cls, obj, location):
+	def at(cls, context, obj, location):
 		if location.at != None:
 			return location.at(obj)
 		else:
-			return 0
+			return {}
 
 	@classmethod
-	def clear_pos(cls, obj, other_objs):
+	def clear_pos(cls, context, obj, other_objs):
 		if len(other_objs) > 0:
 			return 1 - sp.Max(*[cls.on(o, obj) for o in other_objs])
 		else:
 			return 1
 
 	@classmethod
-	def on(cls, obj, location):
+	def on(cls, context, obj, location):
 		if not DLRigidObject.is_a(obj):
 			print('"on" expression can only be generated for things which are RigidObject.')
-			return 0
+			return {}
 
 		if not DLPhysicalThing.is_a(location):
 			print('"on" expression can only be generated for locations which are PhysicalThing.')
-			return 0
+			return {}
 
 		if DLCylinder.is_a(obj) or DLCube.is_a(obj):
 			if DLCube.is_a(location):
 				l2o = pos_of(obj.pose) - pos_of(location.pose)
-				z_align = abs(l2o[2] - obj.height * 0.5 - location.height * 0.5) / 0.2
-				inside  = saturate(abs(dot(l2o, x_col(location.pose)) / location.width - 1) + abs(dot(l2o, y_col(location.pose)) / location.length - 1))
-				return 1 - inside - z_align
+				z_align = obj.height * 0.5 + location.height * 0.5 - l2o[2]
+				inxy = max(abs(dot(l2o, x_col(location.pose))) - location.length * 0.5, 0) + max(abs(dot(l2o, y_col(location.pose))) - location.width * 0.5, 0)
+				in_sc = SC(-inxy, -inxy, 1, inxy)
+				z_sc = SC(-z_align, -z_align, 1, z_align)
+				return {'inside_rect': in_sc, 'above_cube': z_sc}
 			elif DLCylinder.is_a(location):
-				return 0
+				raise Exception('Needs update to produce inequality constraints')
+				return {}
 			elif DLSphere.is_a(location):
-				return 1 - abs(pos_of(obj.pose)[2] - obj.height * 0.5 - pos_of(location.pose)[2] - location.radius) / 0.2
+				expr = pos_of(location.pose)[2] + location.radius - pos_of(obj.pose)[2] - obj.height * 0.5
+				return {'on_sphere': SC(-expr, -expr, 1, expr)}
 		elif DLSphere.is_a(obj):
 			if DLCube.is_a(location):
 				l2o = pos_of(obj.pose) - pos_of(location.pose)
-				z_align = abs(l2o[2] - obj.radius - location.height * 0.5) / 0.2
-				inside  = saturate(abs(dot(l2o, x_col(location.pose)) / location.width - 1) + abs(dot(l2o, y_col(location.pose)) / location.length - 1))
-				return 1 -  norm(l2o) #inside - z_align
+				z_align = obj.radius + location.height * 0.5 - l2o[2]
+				inxy = max(abs(dot(l2o, x_col(location.pose))) - location.length * 0.5, 0) + max(abs(dot(l2o, y_col(location.pose))) - location.width * 0.5, 0)
+				in_sc = SC(-inxy, -inxy, 1, inxy)
+				z_sc = SC(-z_align, -z_align, 1, z_align)
+				return {'inside_rect': in_sc, 'above_cube': z_sc}
 			elif DLCylinder.is_a(location):
-				return 0
+				return {}
 			elif DLSphere.is_a(location):
-				return 1 - abs(pos_of(obj.pose)[2] - obj.radius - pos_of(location.pose)[2] - location.radius) / 0.2
+				expr = pos_of(location.pose)[2] + location.radius - pos_of(obj.pose)[2] - obj.radius
+				return {'on_sphere': SC(-expr, -expr, 1, expr)}
 
 		print('Classification of {} or {} for "on" expression generator has failed'.format(str(obj), str(location)))
-		return 0
+		return {}
 
 	@classmethod
-	def below(cls, obj, location):
+	def below(cls, context, obj, location):
+		raise Exception('Needs update to produce inequality constraints')
 		if location.below != None:
 			return location.below(obj)
 		else:
-			return 0
+			return {}
 
 	@classmethod
-	def inside(cls, obj, volume):
+	def inside(cls, context, obj, volume):
 		if volume.inside != None:
 			return volume.inside(obj)
 		else:
-			return 0
+			return {}
 
 	@classmethod
-	def uniform_space_projection(cls, obj, x, z):
+	def uniform_space_projection(cls, context, obj, x, z):
+		raise Exception('Needs update to produce inequality constraints')
 		frame = x.row_join(cross(x,z)).row_join(z).row_join(pos_of(obj.frame)).inv()
 		obj_x = frame * (x_col(obj.frame) * obj.dimensions[0])
 		obj_y = frame * (y_col(obj.frame) * obj.dimensions[1])
@@ -79,36 +94,137 @@ class SpacialRelations(object):
 
 
 	@classmethod
-	def right_of(cls, obj_a, obj_b, forward=unitX, up=unitZ):
-		projection = cls.uniform_space_projection(obj_b, forward, z)
-		obj_p_pos = projection * pos_of(obj_a.frame)
-		right_rating = -(obj_p_pos[1]**2) - 3 * obj_p_pos[1] - 1.25
-		closeness    = saturate(-(obj_p_pos[0]**2 * obj_p_pos[2]**2) + 1)
-		return right_rating * closeness
+	def max_aabb_expr(cls, obj, frame):
+		if DLCylinder.is_a(obj):
+			cz = z_col(obj.pose)
+			cylinder_up_if = vec3(dot(cz, x_col(frame)), dot(cz, y_col(frame)), dot(cz, z_col(frame)))
+			alpha = sp.acos(abs(cylinder_up_if[2]))
+			beta  = crappyAtan2(abs(cylinder_up_if[0]) + 0.0001, abs(cylinder_up_if[1]))
+			cosA  = sp.cos(alpha)
+			icosA = sp.sin(alpha)
+			cosB  = sp.cos(beta)
+			icosB = sp.sin(beta)
+			z = cosA * 0.5 * obj.height + icosA * obj.radius
+			y = cosB *  (icosA * 0.5 * obj.height + cosA * obj.radius) + icosB * obj.radius
+			x = icosB * (icosA * 0.5 * obj.height + cosA * obj.radius) + cosB * obj.radius
+			return vec3(x,y,z)
+		elif DLCube.is_a(obj):
+			bz = z_col(obj.pose)
+			box_up_if = vec3(dot(bz, x_col(frame)), dot(bz, y_col(frame)), dot(bz, z_col(frame)))
+			alpha = sp.acos(abs(box_up_if[2]))
+			beta  = crappyAtan2(abs(box_up_if[0]) + 0.0001, abs(box_up_if[1]))
+			gamma = sp.acos(abs(dot(z_col(frame), y_col(obj.pose))))
+			cosA  = sp.cos(alpha)
+			icosA = sp.sin(alpha)
+			cosB  = sp.cos(beta)
+			icosB = sp.sin(beta)
+			cosC  = sp.cos(gamma)
+			icosC = 1.0 - cosC
+			up_extend    = cosC * obj.width + icosC * obj.depth
+			depth_extend = cosC * obj.depth + icosC * obj.width
+			z = cosA * 0.5 * obj.height + icosA * obj.radius
+			y = icosB * (icosA * 0.5 * obj.height + cosA * depth_extend) + cosB * up_extend
+			x = cosB  * (icosA * 0.5 * obj.height + cosA * up_extend) + icosB * depth_extend
+			return vec3(x,y,z)
+		elif DLSphere.is_a(obj):
+			return vec3(obj.radius, obj.radius, obj.radius)
+		raise Exception('Failed to construct AABB for object')
 
 	@classmethod
-	def left_of(cls, obj_a, obj_b, forward=unitX, up=unitZ):
-		projection = cls.uniform_space_projection(obj_b, forward, z)
-		obj_p_pos = projection * pos_of(obj_a.frame)
-		left_rating = -(obj_p_pos[1]**2) + 3 * obj_p_pos[1] - 1.25
-		closeness    = saturate(-(obj_p_pos[0]**2 * obj_p_pos[2]**2) + 1)
-		return left_rating * closeness
+	def next_to(cls, context, obj_a, obj_b, observer):
+		if not DLRigidObject.is_a(obj_a):
+			raise Exception('NextTo relation is only defined for rigid objects. However object A is not one.')
+
+		if not DLRigidObject.is_a(obj_b):
+			raise Exception('NextTo relation is only defined for rigid objects. However object B is not one.')
+
+		if DLCube.is_a(obj_a):
+			max_extent_a = max(obj_a.width, obj_b.height, obj_a.depth)
+		elif DLCylinder.is_a(obj_a):
+			max_extent_a = max(obj_b.height, obj_a.radius)
+		elif DLSphere.is_a(obj_a):
+			max_extent_a = obj_a.radius
+
+		if DLCube.is_a(obj_b):
+			max_extent_b = max(obj_b.width, obj_b.height, obj_b.depth)
+		elif DLCylinder.is_a(obj_b):
+			max_extent_b = max(obj_b.height, obj_b.radius)
+		elif DLSphere.is_a(obj_b):
+			max_extent_b = obj_b.radius
+
+
+		d = norm(pos_of(obj_a.pose) - pos_of(obj_b.pose))
+		return {'next_to': SC(-d, max(max_extent_a, max_extent_b) - d, 1, d)}
+
 
 	@classmethod
-	def in_front_of(cls, obj_a, obj_b, forward=unitX, up=unitZ):
-		projection = cls.uniform_space_projection(obj_b, forward, z)
-		obj_p_pos = projection * pos_of(obj_a.frame)
-		front_rating = -(obj_p_pos[0]**2) - 3 * obj_p_pos[0] - 1.25
-		closeness    = saturate(-(obj_p_pos[1]**2 * obj_p_pos[2]**2) + 1)
-		return front_rating * closeness
+	def right_of(cls, context, obj_a, obj_b, observer):
+		if not DLRigidObject.is_a(obj_b):
+			raise Exception('RightOf relation is only defined for rigid objects')
+
+		obs_frame = observer.frame_of_reference
+		b_hdim_in_obs = cls.max_aabb_expr(obj_b, obs_frame)
+		a_hdim_in_obs = cls.max_aabb_expr(obj_a, obs_frame)
+
+		b2a = pos_of(obj_a.pose) - pos_of(obj_b.pose)
+		b2a_in_obs = vec3(dot(b2a, x_col(obs_frame)), dot(b2a, y_col(obs_frame)), dot(b2a, z_col(obs_frame)))
+
+		max_depth = b_hdim_in_obs[0]
+		min_depth = -max_depth
+
+		max_height = b_hdim_in_obs[2]
+		min_height = -max_height
+
+		out = cls.next_to(obj_a, obj_b, observer)
+		out.update({'right_of': SC(-1000, -b_hdim_in_obs[1] - b2a_in_obs[1], 1, b2a_in_obs[1]),
+					'same_depth': SC(min_depth - b2a_in_obs[0], max_depth - b2a_in_obs[0], 1, b2a_in_obs[0]),
+					'same_height': SC(min_height - b2a_in_obs[2], max_height - b2a_in_obs[2], 1, b2a_in_obs[2])})
+		return out
 
 	@classmethod
-	def behind(cls, obj_a, obj_b, forward=unitX, up=unitZ):
-		projection = cls.uniform_space_projection(obj_b, forward, z)
-		obj_p_pos = projection * pos_of(obj_a.frame)
-		back_rating = -(obj_p_pos[0]**2) + 3 * obj_p_pos[0] - 1.25
-		closeness    = saturate(-(obj_p_pos[1]**2 * obj_p_pos[2]**2) + 1)
-		return back_rating * closeness
+	def left_of(cls, context, obj_a, obj_b, observer):
+		if not DLRigidObject.is_a(obj_b):
+			raise Exception('LeftOf relation is only defined for rigid objects')
 
+		obs_frame = observer.frame_of_reference
+		b_hdim_in_obs = cls.max_aabb_expr(obj_b, obs_frame)
+		a_hdim_in_obs = cls.max_aabb_expr(obj_a, obs_frame)
+
+		b2a = pos_of(obj_a.pose) - pos_of(obj_b.pose)
+		b2a_in_obs = vec3(dot(b2a, x_col(obs_frame)), dot(b2a, y_col(obs_frame)), dot(b2a, z_col(obs_frame)))
+
+		max_depth = b_hdim_in_obs[0]
+		min_depth = -max_depth
+
+		max_height = b_hdim_in_obs[2]
+		min_height = -max_height
+
+		out = cls.next_to(obj_a, obj_b, observer)
+		out.update({'left_of': SC(b_hdim_in_obs[1] - b2a_in_obs[1], 1000, 1, b2a_in_obs[1]),
+					'same_depth': SC(min_depth - b2a_in_obs[0], max_depth - b2a_in_obs[0], 1, b2a_in_obs[0]),
+					'same_height': SC(min_height - b2a_in_obs[2], max_height - b2a_in_obs[2], 1, b2a_in_obs[2])})
+		return out
+
+	@classmethod
+	def in_front_of(cls, context, obj_a, obj_b, observer):
+		obs_frame = observer.frame_of_reference
+		b2a = pos_of(obj_a.pose) - pos_of(obj_b.pose)
+		b2a_in_obs = vec3(dot(b2a, x_col(obs_frame)), dot(b2a, y_col(obs_frame)), dot(b2a, z_col(obs_frame)))
+		out = cls.next_to(obj_a, obj_b, observer)
+		out['in_front_of'] = SC(-1000, -b2a_in_obs[0], 1, b2a_in_obs[0])
+		return out
+
+	@classmethod
+	def behind(cls, context, obj_a, obj_b, observer):
+		front_dict = cls.in_front_of(context, obj_a, obj_b, observer)
+		fo_sc = front_dict['in_front_of']
+		del front_dict['in_front_of']
+		front_dict['left_of'] = SC(fo_sc.upper, -fo_sc.lower, 1, fo_sc.expression)
+		return front_dict
+
+	@classmethod
+	def upright(cls, context, obj):
+		expr = 1 - dot(unitZ, z_col(obj.pose))
+		return {'upright': SC(-expr, -expr, 1, expr)}
 
 
