813d385d4de5e6171a5cb805e37f5ddc43e954ab
Adrian Röfer <aroefer@uni-bremen.de>
2018-08-17 12:46:29 -0400

    Updated controllers to work with the new giskardpy.symegine_controller base class

--- src/gebsyas/basic_controllers.py
+++ src/gebsyas/basic_controllers.py
@@ -1,157 +1,21 @@
 import rospy
+import traceback
 
 from time import time
 from giskardpy import print_wrapper
-from giskardpy.input_system import FrameInputRPY
-from giskardpy.qp_problem_builder import SoftConstraint
-from giskardpy.symengine_controller import SymEngineController
-import giskardpy.symengine_wrappers as sw
+from gebsyas.bc_controller_wrapper import BCControllerWrapper
 from gebsyas.utils import StampedData
-from gebsyas.dl_reasoning import DLSymbolic
-from sensor_msgs.msg import JointState
-
-class SimpleCartesianController(QPController):
-    """
-    @brief      A simple controller, moving a frame to a 6-DoF pose.
-    """
-    def __init__(self, robot, movable_frame, builder_backend=None, weight=1, logging=print_wrapper):
-        """Constructor. Receives a robot to control and a frame expression to control.
-           Additionally a backend, a weight for the constraints and a custom logger can be supplied.
-        """
-        self.weight = weight
-        self.movable_frame = movable_frame
-        self.feedback = 0.0
-        super(SimpleCartesianController, self).__init__(robot, builder_backend)
-
-    # @profile
-    def add_inputs(self, robot):
-        self.goal_eef = FrameInputRPY('', 'goal')
-
-    # @profile
-    def make_constraints(self, robot):
-        t = time()
-        # start_position = pos_of(start_pose)
-
-        current_pose = self.movable_frame
-        current_rotation = sw.rot_of(current_pose)
-
-        goal_position = self.goal_eef.get_position()
-        goal_rotation = self.goal_eef.get_rotation()
-        # goal_r = goal_rotation[:3,:3].reshape(9,1)
-
-        #pos control
-        dist = sw.norm(sw.pos_of(current_pose) - goal_position)
-
-        #rot control
-        dist_r = 2 - sw.dot(current_rotation[:3, :1], goal_rotation[:3, :1]) - sw.dot(current_rotation[:3, 1:2], goal_rotation[:3, 1:2])
-
-        self.__feedback = 1 - dist - dist_r
-
-        self._soft_constraints['align {} position'.format(eef)] = SoftConstraint(lower=-dist,
-                                                                                 upper=-dist,
-                                                                                 weight=1,
-                                                                                 expression=dist)
-        self._soft_constraints['align {} rotation'.format(eef)] = SoftConstraint(lower=-dist_r,
-                                                                                 upper=-dist_r,
-                                                                                 weight=1,
-                                                                                 expression=dist_r)
-        self._controllable_constraints = robot.joint_constraints
-        self._hard_constraints = robot.hard_constraints
-        self.logging('make constraints took {}'.format(time() - t))
-
-    def set_goal(self, goal):
-        """
-        dict eef_name -> goal_position
-        :param goal_pos:
-        :return:
-        """
-        self.update_observables(self.goal_eef.get_update_dict(*goal))
-
-    def get_next_command(self):
-    	cmd = super(SimpleCartesianController, self).get_next_command()
-    	self.feedback = self.__feedback.subs(self.get_state())
-    	return cmd
 
 
-class SimpleExpressionController(QPController):
-    """
-    @brief      This controller drives a single expression towards a goal value.
-    """
-    def __init__(self, robot, expression, limit, builder_backend=None, weight=1, logging=print_wrapper):
-        """Constructor. Receives a robot to use, an expression to control and a goal value for the expression.
-           Additionally a backend, a weight for the constraints and a custom logger can be supplied.
-        """
-        self.weight = weight
-        if not DLSymbolic().is_a(expression):
-            raise Exception('Simple Expression Controller was given the non-symbolic expression "{}" of type {}'.format(str(expression), str(type(expression))))
-        self.expression = expression
-        self.limit = limit
-        self.feedback = 0.0
-        super(SimpleExpressionController, self).__init__(robot, builder_backend)
-
-    # @profile
-    def add_inputs(self, robot):
-        pass
-
-    # @profile
-    def make_constraints(self, robot):
-        t = time()
-        # start_position = pos_of(start_pose)
-        ctrl = self.limit - self.expression
-
-        self._soft_constraints['converge expression towards {}'.format(self.limit)] = SoftConstraint(lower=ctrl,
-                                                                                 upper=ctrl,
-                                                                                 weight=self.weight,
-                                                                                 expression=self.expression)
-        self._controllable_constraints = robot.joint_constraints
-        self._hard_constraints = robot.hard_constraints
-        self.logging('make constraints took {}'.format(time() - t))
-        #print('Expression: {}'.format(str(self.expression)))
-
-    def set_goal(self, goal):
-        pass
-
-    def get_next_command(self):
-        cmd = super(SimpleExpressionController, self).get_next_command()
-        self.feedback = self.expression.subs(self.get_state())
-        return cmd
-
-class InEqController(QPController):
+class InEqController(BCControllerWrapper):
     """
     @brief      This controller connects a robot to a set of inequality constraints.
     """
-    def __init__(self, robot, ineq_constraints, builder_backend=None, weight=1, logging=print_wrapper):
+    def __init__(self, robot, logging=print_wrapper):
         """Constructor. Receives a robot to use and soft constraints to abide by.
            Additionally a backend, a weight for the constraints and a custom logger can be supplied.
         """
-        self.weight = weight
-        self.ineq_constraints = ineq_constraints
-        self.feedback = 0.0
-        super(InEqController, self).__init__(robot, builder_backend, logging)
-
-    # @profile
-    def add_inputs(self, robot):
-        pass
-
-    # @profile
-    def make_constraints(self, robot):
-        t = time()
-        # start_position = pos_of(start_pose)
-        self._soft_constraints = self.ineq_constraints.copy()
-        self._controllable_constraints = robot.joint_constraints.copy()
-        self._hard_constraints = robot.hard_constraints.copy()
-        self.logging('make constraints took {}'.format(time() - t))
-        #print('Expression: {}'.format(str(self.expression)))
-
-    def set_goal(self, goal):
-        pass
-
-    def get_next_command(self):
-        cmd = super(InEqController, self).get_next_command()
-        return cmd
-
-    def stop(self):
-        pass
+        super(InEqController, self).__init__(robot, logging)
 
 
 class InEqRunner(object):
@@ -211,8 +75,14 @@ class InEqRunner(object):
         self.trajectory_log.append(StampedData(rospy.Time.from_sec((rospy.Time.now() - self.execution_start).to_sec()), joint_state.copy()))
 
         now = rospy.Time.now()
-        self.robot.set_joint_state(joint_state)
-        command = self.controller.get_next_command()
+        self.controller.set_robot_js(joint_state)
+        try:
+            command = self.controller.get_cmd()
+        except Exception as e:
+            self.terminate = True
+            traceback.print_exc()
+            print(e)
+            return
         #print('\n'.join(['{:>20}: {}'.format(name, vel) for name, vel in command.items()]))
         #self.controller.qp_problem_builder.print_jacobian()
         new_feedback = 0
@@ -224,7 +94,7 @@ class InEqRunner(object):
                 self.convergence_timeout = now + self.tlimit_convergence
 
         self.last_feedback = new_feedback
-        self.constraints_met = self.controller.qp_problem_builder.constraints_met(names=self.task_constraints)
+        self.constraints_met = self.controller.qp_problem_builder.constraints_met(names=self.task_constraints, lbThreshold=0.02, ubThreshold=-0.02)
         self.last_update   = now
 
         self.terminate = now >= self.total_timeout or now >= self.convergence_timeout or self.constraints_met
@@ -289,8 +159,8 @@ class InEqFFBRunner(object):
         self.trajectory_log.append(StampedData(rospy.Time.from_sec((rospy.Time.now() - self.execution_start).to_sec()), joint_state.copy()))
 
         now = rospy.Time.now()
-        self.robot.set_joint_state(joint_state)
-        command = self.controller.get_next_command()
+        self.set_robot_js(joint_state)
+        command = self.controller.get_cmd()
         #print('\n'.join(['{:>20}: {}'.format(name, vel) for name, vel in command.items()]))
         #self.controller.qp_problem_builder.print_jacobian()
         new_feedback = 0
@@ -302,7 +172,7 @@ class InEqFFBRunner(object):
                 self.convergence_timeout = now + self.tlimit_convergence
 
         self.last_feedback = new_feedback
-        self.constraints_met = self.controller.qp_problem_builder.constraints_met()
+        self.constraints_met = self.controller.qp_problem_builder.constraints_met(lbThreshold=0.02, ubThreshold=-0.02)
         self.forces_met =  min([abs(joint_state[j].effort) >= t for j, t in self.force_threshold_dict.items()])
 
         self.last_update = now
@@ -322,80 +192,3 @@ def run_ineq_controller(robot, controller,
     runner = InEqRunner(robot, controller, tlimit_total, tlimit_convergence, agent.act, agent.add_js_callback, dt_threshold, task_constraints)
     constraints_met, lf = runner.run()
     return constraints_met, lf, runner.trajectory_log
-
-
-class ConvergenceTimeoutRunner(object):
-    """
-    !DEPRECATED!
-    Runs a controller which uses a numeric feedback value to indicate its progress.
-    """
-    def __init__(self, robot, controller, feedback_attr,
-                 tlimit_total, tlimit_convergence,
-                 f_send_command, f_add_cb,
-                 dt_threshold, total_threshold=1.0):
-        self.robot = robot
-        self.controller = controller
-        self.feedback_attr = feedback_attr
-        self.tlimit_total  = rospy.Duration(tlimit_total)
-        self.tlimit_convergence = rospy.Duration(tlimit_convergence)
-        self.dt_threshold  = dt_threshold
-        self.total_threshold = total_threshold
-        self.f_send_command = f_send_command
-        self.f_add_cb = f_add_cb
-        self.last_feedback = 0
-        self.last_update   = None
-        self.last_feedback_dt = 1.0
-        self.trajectory_log = []
-        self.execution_start = None
-
-    def run(self):
-        now = rospy.Time.now()
-        self.terminate = False
-        self.total_timeout       = now + self.tlimit_total
-        self.convergence_timeout = now + self.tlimit_convergence
-        self.execution_start = now
-
-        self.f_add_cb(self.js_callback)
-
-        while not rospy.is_shutdown() and not self.terminate:
-            pass
-
-        return self.last_feedback, self.last_feedback_dt
-
-    def js_callback(self, joint_state):
-        if self.terminate: # Just in case
-            return
-
-        #print('Received new joint state')
-
-        self.trajectory_log.append(StampedData(rospy.Time.from_sec((rospy.Time.now() - self.execution_start).to_sec()), joint_state.copy()))
-
-        now = rospy.Time.now()
-        self.robot.set_joint_state(joint_state)
-        command = self.controller.get_next_command()
-        new_feedback = getattr(self.controller, self.feedback_attr)
-        #print('\n'.join(['{:>20}: {}'.format(name, vel) for name, vel in command.items()]))
-        #self.controller.qp_problem_builder.print_jacobian()
-
-        if self.last_update != None:
-            dt = now - self.last_update
-            self.last_feedback_dt = (new_feedback - self.last_feedback) / dt.to_sec()
-            #print('   lf: {}\n   nf: {}\nlf_dt: {}\n   dt: {}'.format(str(self.last_feedback), str(new_feedback), str(self.last_feedback_dt), str(dt.to_sec())))
-            if self.last_feedback_dt > self.dt_threshold and new_feedback <= self.total_threshold:
-                self.convergence_timeout = now + self.tlimit_convergence
-
-        self.last_feedback = new_feedback
-        self.last_update   = now
-
-        self.terminate = now >= self.total_timeout or now >= self.convergence_timeout
-
-        self.f_send_command(command)
-
-
-def run_convergence_controller(robot, controller, feedback_attr,
-                 tlimit_total, tlimit_convergence, agent,
-                 dt_threshold, total_threshold=1.0):
-    """Comfort function to instantiate and run a convergence runner."""
-    runner = ConvergenceTimeoutRunner(robot, controller, feedback_attr, tlimit_total, tlimit_convergence, agent.act, agent.add_js_callback, dt_threshold, total_threshold)
-    cm, lf = runner.run()
-    return cm, lf, runner.trajectory_log
---
+++ src/gebsyas/bc_controller_wrapper.py
@@ -0,0 +1,44 @@
+from giskardpy import print_wrapper
+from giskardpy.symengine_controller import SymEngineController
+from giskardpy.god_map import GodMap
+from gebsyas.utils import res_pkg_path
+
+class BCControllerWrapper(SymEngineController):
+    def __init__(self, robot, print_fn=print_wrapper):
+        self.path_to_functions = res_pkg_path('package://gebsyas/.controllers/')
+        self.controlled_joints = []
+        self.hard_constraints = {}
+        self.joint_constraints = {}
+        self.qp_problem_builder = None
+        self.robot = robot
+        self.current_subs = {}
+        self.print_fn = print_fn
+
+
+    def init(self, soft_constraints):
+        free_symbols = set()
+        for sc in soft_constraints.values():
+            for f in sc:
+                if hasattr(f, 'free_symbols'):
+                    free_symbols = free_symbols.union(f.free_symbols)
+        self.set_controlled_joints([j for j in self.robot.get_joint_names() if self.robot.joint_states_input.joint_map[j] in free_symbols])
+        for jc in self.joint_constraints.values():
+            for f in jc:
+                if hasattr(f, 'free_symbols'):
+                    free_symbols = free_symbols.union(f.free_symbols)
+        for hc in self.hard_constraints.values():
+            for f in hc:
+                if hasattr(f, 'free_symbols'):
+                    free_symbols = free_symbols.union(f.free_symbols)
+        print(free_symbols)
+        super(BCControllerWrapper, self).init(soft_constraints, free_symbols, self.print_fn)
+
+    def set_robot_js(self, js):
+        for j, s in js.items():
+            self.current_subs[self.robot.get_joint_symbol_map().joint_map[j]] = s.position
+
+    def get_cmd(self, nWSR=None):
+        return super(BCControllerWrapper, self).get_cmd({str(s): p for s, p in self.current_subs.items()}, nWSR)
+
+    def stop(self):
+        pass
