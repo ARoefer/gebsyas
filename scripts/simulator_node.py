589d956e9977d9bb80c1e38bd55bb0614cf9da00
Adrian Röfer <aroefer@uni-bremen.de>
2018-08-27 18:24:26 -0400

    GMM simulator run script

--- scripts/simulator_node.py
+++ scripts/simulator_node.py
@@ -3,8 +3,8 @@ import rospy
 import pybullet as pb
 import sys
 from iai_bullet_sim.realtime_simulator_node import FixedTickSimulator
-from gebsyas.simulator_plugins import FullPerceptionPublisher
-from iai_bullet_sim.probabilistic_interactive_node import ProbabilisticInteractiveNode
+from gebsyas.simulator_plugins import GMMObjectPublisher
+from gebsyas.probabilistic_interactive_node import ProbabilisticInteractiveNode, ProbabilisticSimulator
 
 if __name__ == '__main__':
     rospy.init_node('gebsyas_realtime_simulator')
@@ -14,8 +14,11 @@ if __name__ == '__main__':
         print('Mode {} not recognized. Options are: "direct" "gui"'.format(mode))
         exit(-1)
 
-    node = FixedTickSimulator(ProbabilisticInteractiveNode)
+    node = FixedTickSimulator(ProbabilisticInteractiveNode, 'gebsyas_bullet_sim', ProbabilisticSimulator)
     node.init_from_rosparam('sim_config', mode=mode)
+    if not node.sim.has_plugin_of_type(GMMObjectPublisher):
+        node.sim.register_plugin(GMMObjectPublisher())
+
     node.run()
 
     while not rospy.is_shutdown():
