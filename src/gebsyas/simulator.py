093f2c7ca12c0b00f3c4102b2146beab2b94012a
Adrian Röfer <aroefer@uni-bremen.de>
2018-08-28 20:57:16 -0400

    Update to work with GMM objects

--- src/gebsyas/simulator.py
+++ src/gebsyas/simulator.py
@@ -26,7 +26,7 @@ class GebsyasSimulator(BasicSimulator):
         elif DLRigidObject.is_a(dl_object):
           pose = dl_object.pose
         elif DLRigidGMMObject.is_a(dl_object):
-          pose = sorted(dl_object.gmm)[0].pose
+          pose = sorted(dl_object.gmm)[-1].pose
 
         Id = None
         if DLIded.is_a(dl_object):
@@ -36,17 +36,17 @@ class GebsyasSimulator(BasicSimulator):
                 return self.create_box([dl_object.length, dl_object.width, dl_object.height],
                                        vec3_to_list(pos_of(pose)),
                                        list(quaternion_from_matrix(pose)),
-                                       dl_object.mass)
+                                       dl_object.mass, name_override=Id)
             elif DLCylinder.is_a(dl_object):
                 return self.create_cylinder(dl_object.radius, dl_object.height,
                                        vec3_to_list(pos_of(pose)),
                                        list(quaternion_from_matrix(pose)),
-                                       dl_object.mass)
+                                       dl_object.mass, name_override=Id)
             elif DLSphere.is_a(dl_object):
                 return self.create_sphere(dl_object.radius,
                                        vec3_to_list(pos_of(pose)),
                                        list(quaternion_from_matrix(pose)),
-                                       dl_object.mass)
+                                       dl_object.mass, name_override=Id)
             else:
                 Exception('Cannot generate Bullet-body for object which is not a sphere, box, or cylinder.\nObject: {}'.format(str(dl_object)))  
         raise Exception('Cannot generate Bullet-body for object which is not rigid. Object: {}'.format(str(dl_object)))
