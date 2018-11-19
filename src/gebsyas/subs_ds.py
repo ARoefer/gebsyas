36aabd06a42c6150e754f7153ffe07fbf4c60651
Adrian Roefer <aroefer@uni-bremen.de>
2018-11-19 01:39:41 +0100

    Added dynamically generated update functions

--- src/gebsyas/subs_ds.py
+++ src/gebsyas/subs_ds.py
@@ -4,22 +4,24 @@ def to_sym(tuple_name):
     return sp.Symbol('__'.join(tuple_name))
 
 class ListStructure(list):
-    def __init__(self, name, *args):
+    def __init__(self, name, rf, *args):
         super(ListStructure, self).__init__(args)
         self.name = name
         self.free_symbols = set()
+        self.rf = rf
         for x in self.__list:
             self.free_symbols = self.free_symbols.union(x.free_symbols) if hasattr(v, 'free_symbols') else self.free_symbols
 
     def subs(self, sdict):
-        return ListStructure(self.name, *[x.subs(sdict) for x in self.__list if hasattr(x, 'subs') else x])
+        return ListStructure(self.name, self.rf, *[x.subs(sdict) for x in self.__list if hasattr(x, 'subs') else x])
 
 
 class Structure(object):
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, rf, **kwargs):
         self.name = name
         self.free_symbols = set()
         self.edges = set(kwargs.keys())
+        self.rf = rf
         for n, v in kwargs:
             setattr(self, n, v)
             self.free_symbols = self.free_symbols.union(v.free_symbols) if hasattr(v, 'free_symbols') else self.free_symbols
@@ -45,10 +47,7 @@ class Structure(object):
         return out
 
     def subs(self, sdict):
-        return Structure(self.name, **{n: (v.subs(sdict) if hasattr(v, 'subs') else v) for n, v in [(k, getattr(self, k)) for k in self.edges]})
-
-
-    def refresh(self, numeric, state):
+        return Structure(self.name, self.rf, **{n: (v.subs(sdict) if hasattr(v, 'subs') else v) for n, v in [(k, getattr(self, k)) for k in self.edges]})
 
 
 from std_msgs.msg import Header, String, Float64, Bool, Int32
@@ -59,6 +58,8 @@ from geometry_msgs.msg import Quaternion as QuaternionMsg
 from geometry_msgs.msg import PoseStamped as PoseStampedMsg
 
 
+def f_blank(o, state):
+    pass
 
 def ros_msg_to_ks(ros_msg, name, state={}):
     """
@@ -72,9 +73,11 @@ def ros_msg_to_ks(ros_msg, name, state={}):
     if t_msg == int or t_msg == float:
         sym = to_sym(name)
         state[sym] = t_msg
-        return sym
+        def rf(o, state):
+            state[sym] = o
+        return sym, rf
     elif t_msg == bool or t_msg == str:
-        return ros_msg
+        return ros_msg, f_blank
     elif t_msg == PoseMsg:
         syms = []
         for x in ['x', 'y', 'z']:
@@ -85,36 +88,76 @@ def ros_msg_to_ks(ros_msg, name, state={}):
             p = to_sym(name + ('orientation', x))
             state[p] = getattr(ros_msg.orientation, x)
             syms.append(p)
-        return frame3_quaternion(*syms)
+
+        def rf(o, state):
+            state[syms[0]] = o.position.x
+            state[syms[1]] = o.position.y
+            state[syms[2]] = o.position.z
+            state[syms[3]] = o.orientation.x
+            state[syms[4]] = o.orientation.y
+            state[syms[5]] = o.orientation.z
+            state[syms[6]] = o.orientation.2
+        return frame3_quaternion(*syms), rf
     elif t_msg == PointMsg:
         syms = []
         for x in ['x', 'y', 'z']:
             p = to_sym(name + (x,))
             state[p] = getattr(ros_msg, x)
             syms.append(p)
-        return point3(*syms)
+
+        def rf(o, state):
+            state[syms[0]] = o.x
+            state[syms[1]] = o.y
+            state[syms[2]] = o.z
+
+        return point3(*syms), rf
     elif t_msg == Vector3Msg:
         syms = []
         for x in ['x', 'y', 'z']:
             p = to_sym(name + (x,))
             state[p] = getattr(ros_msg, x)
             syms.append(p)
-        return vector3(*syms)
+
+        def rf(o, state):
+            state[syms[0]] = o.x
+            state[syms[1]] = o.y
+            state[syms[2]] = o.z
+        
+        return vector3(*syms), rf
     elif t_msg == QuaternionMsg:
         syms = []
         for x in ['x', 'y', 'z', 'w']:
             p = to_sym(name + (x,))
             state[p] = getattr(ros_msg, x)
             syms.append(p)
-        return rotation3_quaternion(*syms)
+
+        def rf(o, state):
+            state[syms[0]] = o.x
+            state[syms[1]] = o.y
+            state[syms[2]] = o.z
+            state[syms[3]] = o.2
+
+        return rotation3_quaternion(*syms), rf
     elif t_msg == list or t_msg == tuple:
-        return ListStructure(name, *[ros_msg_to_ks(ros_msg[x], name + (str(x),), state) for x in range(len(ros_msg))])
+        objs, rfs = zip(*[ros_msg_to_ks(ros_msg[x], name + (str(x),), state) for x in range(len(ros_msg))])
+
+        def rf(o, state):
+            for x in range(len(rfs)):
+                rfs[x](o[x], state)
+
+        return ListStructure(name, rf, *objs)
     else:
         fields = {}
+        rfs = {}
         for field in dir(ros_msg):
             if field[0] == '_':
                 continue
             attr = getattr(ros_msg, field)
             if not callable(attr) and type(attr) != Header:
-                fields[field] = ros_msg_to_ks(getattr(ros_msg, field), name + (field,), state)
-        return Structure(name, **fields)
+                fields[field], rfs[field] = ros_msg_to_ks(getattr(ros_msg, field), name + (field,), state)
+
+        def rf(o, state):
+            for field, f in rfs.items():
+                f(getattr(o, field), state)
+
+        return Structure(name, rf, **fields)
